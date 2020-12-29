cf39209900595aeaf1821e017df273297c399dfc
darkMarcus <askardatkambek@gmail.com>
2020-12-29 20:08:52 +0600

    second commit

--- src/bitlab/askar/module1/lesson11/Main.java
+++ src/bitlab/askar/module1/lesson11/Main.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String []args){
 
+
+        System.out.println("HEllo");
+
+
         Multivarka multivarka = new Multivarka();
         ElectroCar car = new ElectroCar();
         Telephone telephone = new Telephone();
